d1e2904946d467fad29c59e5b7422af7b06f6d77
Dino Zvonar <62692198+DinoDominicZvonar@users.noreply.github.com>
2026-02-26 23:50:34 +0100

    Modified the site a wee bit

--- code.js
+++ code.js
@@ -1,57 +1,17 @@
-function disableAllElements(){
-    var tabs = document.getElementsByClassName("tab");
-
-    for (var i = tabs.length - 1; i >= 0; i--)
-    {
-        if (!tabs[i].classList.contains("disabled"))
-        {
-            tabs[i].classList.add("disabled");
-        }
-    };
-
-    var contents = document.getElementsByClassName("tabcontent");
-
-    for (var i = contents.length - 1; i >= 0; i--)
-    {
-        contents[i].classList.add("hidden");
-    };
+function HomeClick(el) 
+{
+    if (!el.classList.contains("disabled")) return;
+    window.open("https://dinozvonar.com/");
 }
 
 function AboutClick(el) 
 {
-    disableAllElements();
-    el.classList.remove("disabled");
-
-    var elements = document.getElementsByClassName("aboutme");
-
-    for (var i = elements.length - 1; i >= 0; i--)
-    {
-        elements[i].classList.remove("hidden");
-    };
+    if (!el.classList.contains("disabled")) return;
+    window.open("https://dinozvonar.com/About");
 }
 
 function GamesClick(el) 
 {
-    disableAllElements();
-    el.classList.remove("disabled");
-
-    var elements = document.getElementsByClassName("games");
-
-    for (var i = elements.length - 1; i >= 0; i--)
-    {
-        elements[i].classList.remove("hidden");
-    };
-}
-
-function BlogClick(el) 
-{
-    disableAllElements();
-    el.classList.remove("disabled");
-
-    var elements = document.getElementsByClassName("blog");
-
-    for (var i = elements.length - 1; i >= 0; i--)
-    {
-        elements[i].classList.remove("hidden");
-    };
+    if (!el.classList.contains("disabled")) return;
+    window.open("https://dinozvonar.com/Games");
 }
