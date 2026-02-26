425c92087a73f22c7237dc551bed089e55f5718e
Dino Zvonar <62692198+DinoDominicZvonar@users.noreply.github.com>
2026-02-26 23:58:16 +0100

    Fixed not being able to use the navbar in the about page and the navbar opening new tabs instead of changing the existing tab

--- code.js
+++ code.js
@@ -1,17 +1,17 @@
 function HomeClick(el) 
 {
     if (!el.classList.contains("disabled")) return;
-    window.open("https://dinozvonar.com/");
+    window.location.href = "https://dinozvonar.com/";
 }
 
 function AboutClick(el) 
 {
     if (!el.classList.contains("disabled")) return;
-    window.open("https://dinozvonar.com/About");
+    window.location.href = "https://dinozvonar.com/About";
 }
 
 function GamesClick(el) 
 {
     if (!el.classList.contains("disabled")) return;
-    window.open("https://dinozvonar.com/Games");
+    window.location.href = "https://dinozvonar.com/Games";
 }
